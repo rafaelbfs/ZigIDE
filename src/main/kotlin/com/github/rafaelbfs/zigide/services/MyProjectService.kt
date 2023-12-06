9fdb372a0d928e6323374f82d0ed568def777b42
GitHub Action <action@github.com>
2023-12-06 15:43:17 +0000

    Template cleanup

--- src/main/kotlin/com/github/rafaelbfs/zigide/services/MyProjectService.kt
+++ src/main/kotlin/com/github/rafaelbfs/zigide/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.rafaelbfs.zigide.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.rafaelbfs.zigide.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
